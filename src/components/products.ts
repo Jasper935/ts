f47d18f932eed0aa64e438527fd4a9aae97ad71a
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-09-28 02:33:18 +0300

    u

---
+++ src/components/products.ts
@@ -0,0 +1,8 @@
+import React from "react"
+export function Products(props:any) {
+  return (
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2 ">
+      
+    </div>
+  );
+}
